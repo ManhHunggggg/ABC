992d56b02614705a9804724da6258178e4b4ecf9
ManhHunggggg <91139509+ManhHunggggg@users.noreply.github.com>
2021-09-21 22:56:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hi đằng ấy na',
+    introDesc: ` Đằng ấy thấy tớ xinh gái hơm `,
+    btnIntro: 'Xinh...hihi',
+    title: ' Nêú mà đằng ấy nói dối là phải iu tớ suốt đời đó😸😸😸 ',
+    desc: '♥️♥️♥️♥️♥️♥️♥️♥️♥️♥️♥️ ',
+    btnYes: 'Xinhhhhhh <33',
+    btnNo: 'Xâú vcl :3',
+    question:'Vưà nãy nghe cậu trả lời hơi miễn cưỡng... Cho cậu nói lại đoá😁 <3',
+    btnReply: 'Gửi cho Hoa',
+    reply: 'Xinh thật mà <33333333',
+    mess: 'Ahihi , tớ biết mà🤣🤣🤣',
+    messDesc: 'Add FB Hoa nè.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100070693515932'
 }
