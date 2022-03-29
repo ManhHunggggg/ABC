5b77737896641cb64a1ede704086688e8fb15dae
ManhHunggggg <91139509+ManhHunggggg@users.noreply.github.com>
2022-03-29 20:00:53 +0700

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,20 @@
+
+   audio = new Audio("https://hungdeptrai.com/Two Steps From Hell - Evergreen Extended.mp3");
+
+document.onclick = function() {
+  audio.play();
+  if (typeof audio.loop == 'boolean')
+{
+    audio.loop = true;
+}
+else
+{
+    audio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+}
+audio.play();
+  
+}
+         
